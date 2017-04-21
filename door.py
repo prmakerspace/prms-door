97b44eb28940c2e2cbdf9ff75fd2ef4151f066e9
Thomas Gray <tgray.projects@gmail.com>
2017-04-21 01:21:54 +0000

    Add door.ini dist version

--- door.py
+++ door.py
@@ -4,10 +4,14 @@
 import time
 import pigpio
 import sys
-sys.path.append('../MFRC522-python')
 import MFRC522
 import RPi.GPIO as GPIO
 import signal
+import MySQLdb
+import ConfigParser
+
+# system config
+lock_timeout = 15  # timeout in seconds
 
 # servo config
 servo_gpio   = 14     # Servos connected to gpio 14
@@ -20,6 +24,9 @@ pi = pigpio.pi() # Connect to local Pi.
 pi.set_mode(servo_gpio, pigpio.OUTPUT)
 
 continue_reading = True
+
+config = ConfigParser.ConfigParser()
+config.read("door.ini")
 
 def door_lock():
   print("Locking..")
@@ -34,10 +41,16 @@ def door_unlock():
 # capture SIGINT for cleanup when script is aborted
 def end_read(signal,frame):
   global continue_reading
-  print "Ctrl+C captured, ending read, locking door."
+  print ("Ctrl+C captured, ending read, locking door.")
   continue_reading = False;
   door_lock()
   GPIO.cleanup()
+  db.close()
+
+# for use with a motion sensor, button, or something else for exiting
+# for now, just card swipe out.
+def get_inside_input():
+  return False
 
 # hook the SIGINT
 signal.signal(signal.SIGINT, end_read)
@@ -47,16 +60,23 @@ cardreader = MFRC522.MFRC522()
 # rotate to locked position when powered on initially
 door_lock()
 
+print("sql server: " + config.get('db', 'server'))
+
+db   = MySQLdb.connect(
+  config.get('db', 'server'),
+  config.get('db', 'user'),
+  config.get('db', 'password'),
+  config.get('db', 'database')
+)
+db.autocommit(True)
+curs = db.cursor()
+
 while(continue_reading):
   valid_card = False
 
   # scan for cards
   (status, TagType) = cardreader.MFRC522_Request(cardreader.PICC_REQIDL)
-
-  if status == cardreader.MI_OK:
-    print "Card detected"  
-
-  (status, uid)     = cardreader.MFRC522_Anticoll()
+  (status, uid) = cardreader.MFRC522_Anticoll()
 
   if status == cardreader.MI_OK:
     uid_string = str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
@@ -65,20 +85,39 @@ while(continue_reading):
     key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
 
     # select the scanned tag
-    cardreader.MFRC522_SelectTag(uid)
+    cardreader.MFRC522_SelectTag(uid, False)
 
     status = cardreader.MFRC522_Auth(cardreader.PICC_AUTHENT1A, 8, key, uid)
 
     # @TODO: lookup info in db
     if status == cardreader.MI_OK:
-      cardreader.MFRC522_Read(8)
+      data = cardreader.MFRC522_Read(8, False)
+      print(data)
       cardreader.MFRC522_StopCrypto1()
-      valid_card = True
+      memberId = ''.join(map(str, data[len(data)/2:]))
+      print(memberId)
 
-  # @TODO: trigger on motion sensor
-  motion_sensor = False
+      try:
+        curs.execute("""SELECT account_status, name FROM thelist WHERE id=%s""", (memberId,))
+      except Exception as e:
+        print ("Error - could not fetch updated date/time from member list for id " + memberId)
+        print(str(e))
+        
+      if curs.rowcount==1:
+        account_status, name = curs.fetchone()
+        if str(account_status) == 'Active':
+          valid_card = True
+          curs.execute("""INSERT INTO event_log VALUES (%s, %s, CURRENT_TIMESTAMP)""",(memberId, 'card swipe'))
+        else:
+          print("status is not 'Active' - is " + str(account_status))
+      else:
+        print("Could not find member in list")
+          
+
+  # @TODO: trigger on inside input
+  inside_input = get_inside_input()
   
-  if (valid_card or motion_sensor):
+  if (valid_card or inside_input):
     # @TODO: record video on trigger
     # unlock
     print("Valid input.")
@@ -86,7 +125,7 @@ while(continue_reading):
 
     # @TODO: handle door being open - needs a sensor
     # motion sensor, maybe?  When motion stops, assume closed?
-    time.sleep(30)
+    time.sleep(lock_timeout)
 
     # lock again
     print("Relocking.")
